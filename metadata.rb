cd8a1d471308221a1d486f9ac7b3b717f55034e9
Bastian Schwarz <bastian@codename-php.de>
2018-02-11 19:46:15 +0100

    Updated readme, version and changelog for release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'bastian@codename-php.de'
 license 'Apache-2.0'
 description 'Cookbook to install php'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.0'
+version '1.1.0'
 chef_version '>= 13.0' if respond_to?(:chef_version)
 issues_url 'https://github.com/codenamephp/chef.cookbook.php/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/codenamephp/chef.cookbook.php' if respond_to?(:source_url)
